2a01ce6d4a91073c59b6d8b27fab9c6bff1237c0
myroslav2811 <35901380+myroslav2811@users.noreply.github.com>
2019-08-02 21:15:16 +0300

    Add files via upload

---
+++ precache-manifest.d493056c9ba77aefa6fa1bc51f860e26.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3072d37020fd89a1f180aaee807b7a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac28b6d16c13cd652bd6",
+    "url": "/static/css/2.1870a124.chunk.css"
+  },
+  {
+    "revision": "14796b916dea640ee391",
+    "url": "/static/css/main.3b9c945c.chunk.css"
+  },
+  {
+    "revision": "ac28b6d16c13cd652bd6",
+    "url": "/static/js/2.03167264.chunk.js"
+  },
+  {
+    "revision": "14796b916dea640ee391",
+    "url": "/static/js/main.99c81482.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
